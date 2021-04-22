920337d234bb0542961d15313215d09500899160
aidil <aidilhaqeemy@gmail.com>
2021-04-22 14:06:40 +0800

    add looping

--- 2.3.c
+++ 2.3.c
@@ -5,33 +5,24 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void childTask()
-{
-	printf("Salam, saya anak tau\n");
-}
-
-void parentTask()
-{
-	printf("Dan saya adalah bapanya\n");
-}
-
 int main(void)
 {
-	pid_t pid = fork();
+  for(int i=1; i<13; i++)
+  {
+    pid_t pid = fork();
 
-	if (pid == 0)
-	{
-		childTask();
-		exit(EXIT_SUCCESS);
-	}
-	else if (pid > 0)
-	{
-		wait(NULL);
-		parentTask();
-	}
-	else
-	{
-		printf("Unable to create child process.");
-	}
-	return EXIT_SUCCESS;
+    if(pid == 0)
+    {
+      printf("Child process => PPID=%d, PID=%d\n", getppid(), getpid());
+      exit(0);
+    }
+    else
+    {
+      printf("Parent process => PID=%d\n", getpid());
+      printf("Waiting for the child processes to finish...\n");
+      wait(NULL);
+      printf("child process finished.\n");
+    }
+  }
+  return EXIT_SUCCESS;
 }
