b4c28b9035eff715ac85cccede3677a1f93c6586
aidil <aidilhaqeemy@gmail.com>
2021-05-02 15:52:04 +0800

    editing my coding

--- 2.4.c
+++ 2.4.c
@@ -4,29 +4,36 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+char name;
+
+void childTask()
+{
+  printf("Enter your name: ");
+  scanf("%s", &name);
+  printf("The child name is %s\n", &name);
+}
+
 int main(void)
 {
   for(int i=1; i<5; i++)
   {
         pid_t pid = fork();
-	char name;
 
 
 	if(pid == 0)
 	{
-		printf("Enter your name: ");
-		scanf("%s", &name);
-		printf("The child name is %s\n", &name);
+		childTask();
+		exit(EXIT_SUCCESS);
 	}
 	else if(pid > 0)
 	{
 		wait(NULL);
-		printf("Job is done\n");
 	}
 	else
 	{
 		printf("error404");
 	}
   }
-  return 0;
+  printf("Job is done!\n");
+  return EXIT_SUCCESS;
 }
